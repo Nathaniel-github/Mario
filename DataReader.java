81cd3e5a9344d52c6a11d61836731117faba520d
Lumijek123 <lakaayush@gmail.com>
2020-05-20 12:34:12 -0700

    Fixed bugs and added props

--- DataReader.java
+++ DataReader.java
@@ -22,6 +22,9 @@ public class DataReader {
 	private LinkedList<String[]> allLongPipes = new LinkedList<String[]>();
 	private LinkedList<String[]> allGoombas = new LinkedList<String[]>();
 	private LinkedList<String[]> allKoopas = new LinkedList<String[]>();
+	private LinkedList<String[]> allLongTrees = new LinkedList<String[]>();
+	private LinkedList<String[]> allShortTrees = new LinkedList<String[]>();
+	private LinkedList<String[]> allGates = new LinkedList<String[]>();
 
 	
 	public DataReader(String fileName) {
@@ -83,6 +86,18 @@ public class DataReader {
 			} else if (allData.get(i)[0].equals("Koopa")) {
 				
 				allKoopas.add(allData.get(i));
+				
+			} else if (allData.get(i)[0].equals("LongTree")) {
+				
+				allLongTrees.add(allData.get(i));
+				
+			} else if (allData.get(i)[0].equals("ShortTree")) {
+				
+				allShortTrees.add(allData.get(i));
+				
+			} else if (allData.get(i)[0].equals("Gate")) {
+				
+				allGates.add(allData.get(i));
 				
 			}
 			
@@ -185,6 +200,24 @@ public class DataReader {
 	public LinkedList<String[]> getAllKoopas() {
 		
 		return allKoopas;
+		
+	}
+	
+	public LinkedList<String[]> getAllLongTrees() {
+		
+		return allLongTrees;
+		
+	}
+	
+	public LinkedList<String[]> getAllShortTrees() {
+		
+		return allShortTrees;
+		
+	}
+	
+	public LinkedList<String[]> getAllGates() {
+		
+		return allGates;
 		
 	}
 	
---
+++ LongTree.java
@@ -0,0 +1,69 @@
+import java.awt.Image;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class LongTree implements Prop{
+	// Fields
+	private final Image IMAGE;
+	private final ImageIcon IMAGEICON;
+	private final int XCORD;
+	private final int YCORD;
+	
+	public LongTree(int x, int y) {
+		
+		// Save x and y coordinates given when instantiated
+		XCORD = x;
+		YCORD = y;
+		// Gets the ImageIcon of the short pipe
+		IMAGEICON = new ImageIcon(getClass().getClassLoader().getResource("PropImages/LongTree.png"));
+		// Gets the image from the ImageIcon
+		IMAGE = IMAGEICON.getImage();
+		
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return IMAGE;
+	}
+
+	@Override
+	public int getXCord() {
+		// TODO Auto-generated method stub
+		return XCORD;
+	}
+
+	@Override
+	public int getYCord() {
+		// TODO Auto-generated method stub
+		return YCORD;
+	}
+
+	@Override
+	public Icon getImageIcon() {
+		// TODO Auto-generated method stub
+		return IMAGEICON;
+	}
+
+	@Override
+	public Rectangle getRectangle() {
+		// TODO Auto-generated method stub
+		return new Rectangle(XCORD, YCORD, IMAGEICON.getIconWidth(), IMAGEICON.getIconHeight());
+	}
+
+	@Override
+	public boolean isObstructive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Polygon getCollider() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
